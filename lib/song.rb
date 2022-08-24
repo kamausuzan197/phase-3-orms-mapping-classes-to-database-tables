c7f3689cd6e4c1b1e797eac3b0eb290f876eb128
kamausuzan197 <kamaususan197@gmail.com>
2022-08-24 08:22:11 +0300

    copmlete

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,48 @@
 class Song
 
-  attr_accessor :name, :album
+  attr_accessor :id, :name, :album
 
-  def initialize(name:, album:)
+  def initialize(id:nil, name:, album:)
+    @id = id
     @name = name
     @album = album
   end
 
+  def self.create_table
+    sql =  <<-SQL
+      CREATE TABLE IF NOT EXISTS songs (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        album TEXT
+      )
+      SQL
+    DB[:conn].execute(sql)
+  end
+  
+  def save
+    #For strings that will take up multiple lines in your text editor, use a heredoc (Links to an external site.) to create a string that runs on to multiple lines. <<- + special word meaning "End of Document" + the string, on multiple lines + special word meaning "End of Document". You don't have to use a heredoc, it's just a helpful tool for crafting long strings in Ruby. 
+    #Back to our regularly scheduled programming...
+    sql = <<-SQL
+    INSERT INTO songs (name, album)
+    VALUES (?, ?)
+    SQL
+    #Instead of interpolating variables into a string of SQL, we are using the ? characters as placeholders.
+#call the variable db to update the data.
+    DB[:conn].execute(sql, self.name, self.album)
+
+    #get the song ID from the database and save it to the ruby instance
+    self.id= DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
+    #return the ruby instance.
+  self
+  end
+
+  def self.create(name:, album:)
+    song=Song.new(name:name, alum: album)
+    song.save
+  end
 end
+
+azimimio =Song.new(name: "Gold Digger", album: "Late Registration")
+ puts azimimio.name
+ puts azimimio.album
+
